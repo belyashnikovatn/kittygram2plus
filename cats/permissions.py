0af295a2edb7a6302411bad75f8e96814b30a76a
Tania Belyashnikova <belyashnikova.tn@gmail.com>
2024-07-08 08:49:08 +0500

    change permissions

--- cats/permissions.py
+++ cats/permissions.py
@@ -10,10 +10,12 @@ class OwnerOrReadOnly(permissions.BasePermission):
         )
 
     def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return obj.owner == request.user
 
 
-class ReadOnly(permissions.BasePermission):
+# class ReadOnly(permissions.BasePermission):
 
-    def has_permission(self, request, view):
-        return request.method in permissions.SAFE_METHODS
+#     def has_permission(self, request, view):
+#         return request.method in permissions.SAFE_METHODS
--- cats/views.py
+++ cats/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework import permissions
 
 from .models import Achievement, Cat, User
-from .permissions import OwnerOrReadOnly, ReadOnly
+from .permissions import OwnerOrReadOnly
 
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
 
@@ -12,13 +12,13 @@ class CatViewSet(viewsets.ModelViewSet):
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly, )
 
-    def get_permissions(self):
-        if self.action == 'retrieve':
-            return (ReadOnly(), )
-        return super().get_permissions()
+    # def get_permissions(self):
+    #     if self.action == 'retrieve':
+    #         return (ReadOnly(), )
+    #     return super().get_permissions()
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user) 
+        serializer.save(owner=self.request.user)
 
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
